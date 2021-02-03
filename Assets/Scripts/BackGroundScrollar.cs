633ad3274fc4aedb545973f07308629a2ae90c99
ShuminArtem <77163623+ShuminArtem@users.noreply.github.com>
2021-02-03 18:53:24 +0200

    Initial commit

---
+++ Assets/Scripts/BackGroundScrollar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScrollar : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    Material meMaterial;
+    Vector2 offset;
+    void Start()
+    {
+        meMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
